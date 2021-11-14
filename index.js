8a400f719f07c18311b61c8156a5b6b3d5c536f4
Code0Artuh <87547578+code0Artuh@users.noreply.github.com>
2021-11-14 20:28:10 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");  //chamando o express
+const app = express();  //definindo o app como express
+require('dotenv').config();
+
+app.use(express.json());  //definindo o JSON no projeto
+
+const Conn = require("./model/conn/index"); //importando a conexao
+
+Conn(); //executa a func de conexao
+
+const port = 3000; //porta do node
+
+const paisesRouter = require("./routers/paises.routes");
+app.use('/paises',paisesRouter);
+
+const cidadesRouter = require("./routers/cidades.routes");
+app.use('/cidades',cidadesRouter);
+
+const estadosRouter = require("./routers/estados.routes");
+app.use('/estados',estadosRouter);
+
+app.listen(port, () => {
+    console.log(`Servidor rodando em: http://localhost:${port}`);
+});
