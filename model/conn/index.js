81395751137e4103c7573ed394d7b2a259fad7c2
Code0Artuh <87547578+code0Artuh@users.noreply.github.com>
2021-11-14 20:50:14 -0300

    Update index.js

--- model/conn/index.js
+++ model/conn/index.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");  //importando o mongoose
 
 async function Conn(){
-    await mongoose.connect("mongodb+srv://827581354455:827581354455@cluster0.svrkx.mongodb.net/projeto2",{ // string de conexao ou local ou atlas
+    await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_BASE}`,{ // string de conexao ou local ou atlas
         useNewUrlParser: true,
         useUnifiedTopology: true,
     }).then(() => { // tudo certo faz isso
