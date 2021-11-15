5264541364d03c25d00b0e8b9a8cb2b21fe4f62d
Code0Artuh <87547578+code0Artuh@users.noreply.github.com>
2021-11-14 21:21:44 -0300

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const Conn = require("./model/conn/index"); //importando a conexao
 
 Conn(); //executa a func de conexao
 
-const port = process.env.PORT || 3001; //porta do node
+const port = process.env.PORT || 3000; //porta do node
 
 app.get('/', (req,res) => {
     res.status(200).json({message:"API Rodando"});
