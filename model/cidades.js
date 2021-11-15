90ed5db29c9f411f021b42b569a82d98a2932f58
Code0Artuh <87547578+code0Artuh@users.noreply.github.com>
2021-11-14 21:47:13 -0300

    Update cidades.js

--- model/cidades.js
+++ model/cidades.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");  //importando o mongoose
 
 const cidadesModel = new mongoose.Schema({ //criando nosso modelo do banco
     nome: { type: String, required: true }, // chave/ valor: tipo do valor e se é obrigatorio
-    bairros: { type: Number },
-    populacao: { type: Number },
-    aniversario: {type: Number},
+    bairros: { type: String, required: true },
+    populacao: { type: String, required: true },
+    aniversario: {type: String, required: true},
     dataCriacao: { type: Date, default: Date.now } //default, valor padrao caso nao seja passado
 });
 
