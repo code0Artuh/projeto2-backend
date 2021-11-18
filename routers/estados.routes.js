3df4a5f194dd1215c1d275b142c3a92f5fa76097
Code0Artuh <87547578+code0Artuh@users.noreply.github.com>
2021-11-18 04:21:19 -0300

    Update estados.routes.js

--- routers/estados.routes.js
+++ routers/estados.routes.js
@@ -16,9 +16,9 @@ router.get('/listall', async (req,res) => {
     });
 });
 
-router.get('/listname/:id', async (req,res) => {
-    const id = req.params.id;  //recebendo nome por parametro
-    await estado.findOne({ _id:id }).then((estados) => { //findOne retorna o primeiro que der match com o item passado
+router.get('/listname/:nome', async (req,res) => {
+    const id = req.params.nome;  //recebendo nome por parametro
+    await estado.findOne({ nome:id }).then((estados) => { //findOne retorna o primeiro que der match com o item passado
         console.log(estados);
         if(estados == null){ //validando se retorna null 
             res.status(404).json({message: "nao foi encontrado"});
